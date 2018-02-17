a72f9428d6617f912da821b0b36db98973b8cc08
Rutger de Graaf <rutger@whizzkit.nl>
2018-02-17 13:35:45 +0100

    Added saving to MySQL, following structure of ics2000.php

--- public_html/weather.php
+++ public_html/weather.php
@@ -1,15 +1,24 @@
 <?php
 
-require_once 'classes.php';
-require_once 'ES/ESUploader.php';
-require_once 'OWM/owm.php';
+require_once dirname(__FILE__) . '/classes.php';
+require_once dirname(__FILE__) . '/OWM/owm.php';
+
+$conf = json_decode(file_get_contents(dirname(__FILE__) . "/conf/ics2000.json"), true);
+date_default_timezone_set($conf['timezone']);
+
+$iSaveables = array();
+foreach($conf['destinations'] as $iSaveable) {
+    require_once dirname(__FILE__) . '/'.$iSaveable['file'];
+    $iSaveables[]=new $iSaveable['class']();    
+}
 
 $owm = new OWM();
 $response = $owm->get();
-
 $array = json_decode($response, true);
 $array['timestamp'] = gmdate('Y-m-d H:i:s', $array['dt']);
 unset($array['dt']);
+$weather=new Weather($array);
 
-$es = new ESUploader();
-$es->save(new Weather($array));
+foreach($iSaveables as $iSaveable) {
+    $iSaveable->save($weather);
+}
