9fe704e6b906db42a2773e81335024fa2737194d
Rutger de Graaf <rutger@whizzkit.nl>
2018-02-17 13:36:27 +0100

    Moved all config to json file

--- public_html/MySQL/MySQLUploader.php
+++ public_html/MySQL/MySQLUploader.php
@@ -4,34 +4,13 @@ require_once dirname(__FILE__) . '/../ISaveable.php';
 
 class MySQLUploader implements ISaveable {
 
-    private $host;
-    private $port;
-    private $username;
-    private $password;
-    private $database;
     private $mysqli;
-
-    private $conf = array(
-        "Reading" => array(
-            "table" => "energy_reading",
-            "sql" => "conf/reading.sql"
-        ),
-        "Usage" => array(
-            "table" => "energy_usage",
-            "sql" => "conf/usage.sql"
-        )
-    );
 
     function __construct() {
 
-        $conf=json_decode(file_get_contents(dirname(__FILE__) . "/conf/mysql.json"),true);
-        $this->host=$conf['host'];
-        $this->port=$conf['port'];
-        $this->username=$conf['username'];
-        $this->password=$conf['password'];
-        $this->database=$conf['database'];
+        $this->conf=json_decode(file_get_contents(dirname(__FILE__) . "/conf/mysql.json"),true);
 
-        $this->mysqli = new mysqli($this->host, $this->username, $this->password, $this->database,$this->port);
+        $this->mysqli = new mysqli($this->conf['host'], $this->conf['username'], $this->conf['password'], $this->conf['database'],$this->conf['port']);
         if ($this->mysqli->connect_errno) {
             $this->handle_error();
         }
@@ -43,15 +22,27 @@ class MySQLUploader implements ISaveable {
 
     public function save($object) {
         $classname = get_class($object);
-        $sql = "insert into " . $this->conf[$classname]['table'];
+        $sql = "insert into " . $this->conf['tables'][$classname]['table'];
         $columns = "(";
         $values = "(";
         $duplicate = "";
+
+        if(array_key_exists('mapping',$this->conf['tables'][$classname])) {
+            $mapping=$this->build_mapping($this->conf['tables'][$classname]['mapping']);
+            $array=array();
+            foreach ($mapping as $key => $target) {
+                eval("\$value=\$object->$target;");
+                $array[$key]=$value;
+            }
+            $object=new $classname($array);
+        }
+
         foreach ($object as $key => $value) {
             $columns .= ($columns == "(" ? "" : ",") . $key;
             $values .= ($values == "(" ? "" : ",") . (is_numeric($value) ? $value : "'${value}'");
             $duplicate .= ($duplicate == ""?"":",")."${key}=". (is_numeric($value) ? $value : "'${value}'");
         }
+
         $sql .= $columns . ") values " . $values . ") on duplicate key update ".$duplicate;
         if (!$this->mysqli->query($sql)) {
             $this->handle_error();
@@ -59,7 +50,7 @@ class MySQLUploader implements ISaveable {
     }
 
     public function setup() {
-        foreach ($this->conf as $classname => $values) {
+        foreach ($this->conf['tables'] as $classname => $values) {
             echo "Creating table ${values['table']}\n";
             if (!$this->mysqli->query("create table ${values['table']} ".file_get_contents(dirname(__FILE__) . "/${values['sql']}"))) {
                 $this->handle_error();
@@ -68,7 +59,7 @@ class MySQLUploader implements ISaveable {
     }
 
     public function teardown() {
-        foreach ($this->conf as $classname => $values) {
+        foreach ($this->conf['tables'] as $classname => $values) {
             echo "Dropping table ${values['table']}\n";
             if (!$this->mysqli->query("drop table ${values['table']}")) {
                 $this->handle_error();
@@ -80,4 +71,13 @@ class MySQLUploader implements ISaveable {
         echo "Statement failed: (" . $this->mysqli->errno . ") " . $this->mysqli->error . "\n";
     }
 
+    private function build_mapping($values) {
+        $mapping=array();
+        foreach($values as $value) {
+            foreach($value as $k => $v ) {
+                $mapping[$k]=$v;
+            }
+        }        
+        return $mapping;
+    }
 }
